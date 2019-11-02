996887945564e41c6bd7f3e9c696a08e7fdd42fc
masiroro <denniskiprop25.com>
2019-11-02 14:02:49 +0300

    PSK

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+a=("Hello world\n\n")
+
+print(a)
+name=("Stan")
+age=("20+")
+c=(f"My name is {name}.\nI am {age} years of age.Young, Rigth?\n\n")
+print(c)
+d=("The maximum number in this list: [12, 4, 56, 17, 8, 99] is 99")
+f=(12+4+56+17+8+99)/6
+
+e=( f"The mean: [12, 4, 56, 17, 8, 99] is {f} \n\n")
+print(d)
+print(e)
+
+print("A for Apple")
+
+print("B for Boy")
+print("C for Cow")
+
+print("...\n")
+
+print("Z for Zebra")
